8d88e83fcaee668d7bff944112137d259b488ca4
wiechecdawid <wiechec.dawid@gmail.com>
2021-11-28 01:19:23 +0100

    Register viev model, user model adjusted

--- WeLoveBooks.DataAccess/Models/AppUser.cs
+++ WeLoveBooks.DataAccess/Models/AppUser.cs
@@ -4,6 +4,8 @@ namespace WeLoveBooks.DataAccess.Models;
 
 public class AppUser: IdentityUser
 {
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
     public ICollection<Review> Reviews { get; set; }
     public ICollection<Comment> Comments { get; set; }
 }
