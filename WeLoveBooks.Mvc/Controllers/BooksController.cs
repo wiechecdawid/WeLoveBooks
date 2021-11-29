d01c5d69eb6f51c9da4dcd5139c8050cc811e7df
wiechecdawid <wiechec.dawid@gmail.com>
2021-11-29 23:50:27 +0100

    BooksController - context added

--- WeLoveBooks.Mvc/Controllers/BooksController.cs
+++ WeLoveBooks.Mvc/Controllers/BooksController.cs
@@ -1,9 +1,17 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using WeLoveBooks.DataAccess.Data;
 
 namespace WeLoveBooks.Mvc.Controllers
 {
     public class BooksController : Controller
     {
+        private readonly AppDbContext _context;
+
+        public BooksController(AppDbContext context)
+        {
+            _context = context;
+        }
+
         public IActionResult Index()
         {
             return View();
