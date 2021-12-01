7493f75fdeee8802d4a7e708685d4d5705fbde62
wiechecdawid <wiechec.dawid@gmail.com>
2021-12-01 23:40:29 +0100

    Books View added

---
+++ WeLoveBooks.Mvc/Areas/AdminArea/ViewModels/CreateBookViewModel.cs
@@ -0,0 +1,12 @@
+﻿using WeLoveBooks.DataAccess.Models;
+
+namespace WeLoveBooks.Mvc.Areas.AdminArea.ViewModels;
+
+public class CreateBookViewModel
+{
+    public string Title { get; set; }
+    public string Description { get; set; }
+    public DateTime CreatedDate { get; set; }
+    public Author Author { get; set; }
+    public IEnumerable<Author> Authors { get; set; }
+}
