fe77e7ba10fad3621f7b702ffa6a54456b507488
wiechecdawid <wiechec.dawid@gmail.com>
2021-12-18 19:19:59 +0100

    Create book ViewModel

---
+++ WeLoveBooks.Mvc/ViewModels/CreateReviewViewModel.cs
@@ -0,0 +1,14 @@
+﻿using WeLoveBooks.DataAccess.Models;
+
+namespace WeLoveBooks.Mvc.ViewModels;
+
+public class CreateReviewViewModel
+{
+    public string Id { get; set; }
+    public string Title { get; set; }
+    public string Content { get; set; }
+    public Book Book { get; set; }
+    public IEnumerable<Book> Books { get; set; }
+    public string UserName { get; set; }
+    public DateTime CreatedDate { get; set; }
+}
